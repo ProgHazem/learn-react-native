5f7a887f0ca6f2ab12489cc1888622ead11d60e6
Fox <habdelsatar@faydety.com>
2022-06-05 17:30:28 +0200

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [goal, setGoal] = useState("")
+  const [goals, setGoals] = useState<string[]>([])
+  // @ts-ignore
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.body}>
+       <TextInput placeholder="Course Goal" style={styles.input} onChangeText={(e) => setGoal(e)}/>
+       <Button title="Add" onPress={() => setGoals([...goals, goal])}/>
+     </View>
+      {goals && goals > 0 &&
+          <view>
+            {goals?.map((goal) => <Text key={goal}>{goal}</Text>)}
+          </view>
+      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50
   },
+  body: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
